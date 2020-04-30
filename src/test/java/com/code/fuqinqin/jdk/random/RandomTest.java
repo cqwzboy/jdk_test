bb1af900599ee0be440be6da870807260a12b9a9
fuqinqin3 <fuqinqin3@jd.com>
2020-04-30 10:05:22 +0800

    Up

---
+++ src/test/java/com/code/fuqinqin/jdk/random/RandomTest.java
@@ -0,0 +1,19 @@
+package com.code.fuqinqin.jdk.random;
+
+import org.testng.annotations.Test;
+
+/**
+ * 随机数测试类
+ * @author fuqinqin3
+ * @date 2020-04-28
+ * */
+public class RandomTest {
+    @Test
+    public void test(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append(String.valueOf((int) (Math.random()*10)));
+        }
+        System.out.println("#88345 随机数："+sb.toString());
+    }
+}
