ac4a143bda65274aa58469d0db4dbb74d7e6c190
fuqinqin3 <fuqinqin3@jd.com>
2020-11-20 15:27:46 +0800

    8888-11111

--- src/main/java/com/code/fuqinqin/test/Test1.java
+++ src/main/java/com/code/fuqinqin/test/Test1.java
@@ -1,7 +1,9 @@
 package com.code.fuqinqin.test;
 
 public class Test1 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println("3252352352");
     }
+
 }
--- src/main/java/com/code/fuqinqin/test/Test3.java
+++ src/main/java/com/code/fuqinqin/test/Test3.java
@@ -2,6 +2,8 @@ package com.code.fuqinqin.test;
 
 public class Test3 {
     public static void main(String[] args) {
+        String a = "1";
+        System.out.println(a);
 
     }
 }
