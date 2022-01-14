58c3ad4630b3d24af0023d33352666fe657d2a6e
fuqinqin <904571943@qq.com>
2022-01-14 17:01:56 +0800

    JWT Up 2

--- src/main/java/com/code/fuqinqin/jwt/JWTTest.java
+++ src/main/java/com/code/fuqinqin/jwt/JWTTest.java
@@ -3,6 +3,7 @@ package com.code.fuqinqin.jwt;
 import com.alibaba.fastjson.JSONObject;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.badger.common.lang.util.StringUtil;
 
@@ -18,6 +19,13 @@ public class JWTTest {
         String token = generateToken();
 
         // 篡改token信息
+//        token = tampering(token);
+
+        // 解密token
+        decodeToken(token);
+    }
+
+    private static String tampering(String token) throws UnsupportedEncodingException {
         token = new String(Base64.getDecoder().decode(token.getBytes(CHARSET)), CHARSET);
         DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC256(SECRET)).build().verify(token);
         JSONObject jsonObject = JSONObject.parseObject(new String(Base64.getDecoder().decode(decodedJWT.getPayload().getBytes(CHARSET)), CHARSET));
@@ -25,9 +33,7 @@ public class JWTTest {
         token = StringUtil.concat_ws(".", decodedJWT.getHeader(), Base64.getEncoder().encodeToString(jsonObject.toJSONString().getBytes(CHARSET)), decodedJWT.getSignature());
         System.out.println("篡改后的token = " + token);
         token = Base64.getEncoder().encodeToString(token.getBytes(CHARSET));
-
-        // 解密token
-        decodeToken(token);
+        return token;
     }
 
     private static void decodeToken(String token) throws UnsupportedEncodingException {
@@ -44,10 +50,14 @@ public class JWTTest {
         String payload = decodedJWT.getPayload();
         String signature = decodedJWT.getSignature();
         String decodeToken = decodedJWT.getToken();
+        Map<String, Claim> claims = decodedJWT.getClaims();
         System.out.println("header = " + header);
         System.out.println("payload = " + payload);
         System.out.println("signature = " + signature);
         System.out.println("decodeToken = " + decodeToken);
+        System.out.println("claims = " + JSONObject.toJSONString(claims));
+        System.out.println("one = " + decodedJWT.getClaim("one").asInt());
+        System.out.println("one = " + claims.get("one").asInt());
     }
 
     private static String generateToken() throws UnsupportedEncodingException {
